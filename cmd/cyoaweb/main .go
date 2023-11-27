88aa4867da9a66a93a78130b83d7abfe304851eb
souravdey425 <42832220+souravdey425@users.noreply.github.com>
2023-11-27 15:49:09 +0530

    html include

--- cmd/cyoaweb/main .go
+++ cmd/cyoaweb/main .go
@@ -1,15 +1,17 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 
 	"github.com/souravdey425/cyoa"
 )
 
 func main() {
+	port := flag.Int("port", 3000, "port to start the server")
 	fileName := flag.String("file", "gopher.json", "its contain a json file")
 	flag.Parse()
 
@@ -17,8 +19,12 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 	}
-	d := json.NewDecoder(f)
-	var story cyoa.Story
-	d.Decode(&story)
+	story, err := cyoa.JsonStory(f)
+	if err != nil {
+		fmt.Println(err)
+	}
 	fmt.Printf("%+v", story)
+	h := cyoa.NewRequest(story)
+	fmt.Printf("starting the server the port %d\n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
 }
